de6f155967315ec8fbefbdfea59dacc0e8dde010
Giulio Starace <giulio.starace@gmail.com>
2021-02-03 11:42:44 +0100

    basic vegajs working example

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,12 @@
 import * as React from "react";
+import VegaSpec from "../../content/decisioni.vg.json";
+import VegaData from "../../content/flare.json";
+import { Vega } from "react-vega";
 
-export default () => {
-  return <h1>Hello World!</h1>;
-};
+export default () => (
+  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+    <h1>Hello World!</h1>
+    <div>{VegaSpec.description}</div>
+    <Vega spec={VegaSpec} data={VegaData} />
+  </div>
+);
