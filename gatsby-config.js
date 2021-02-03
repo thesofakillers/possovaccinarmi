4f471941ae950eaa2644b189e35675ddc81c144a
Giulio Starace <giulio.starace@gmail.com>
2021-02-03 10:42:53 +0100

    init gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: "Posso Vaccinarmi?",
+  },
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
