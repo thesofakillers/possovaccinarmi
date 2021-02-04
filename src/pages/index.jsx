642762db53ffc4bfaece9fff6b817a446ae670ff
Giulio Starace <giulio.starace@gmail.com>
2021-02-04 13:56:17 +0100

    switch to force-directed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,9 @@ export default () => (
   <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
     <h1>Hello World!</h1>
     <div>{VegaSpec.description}</div>
-    <Vega spec={VegaSpec} data={VegaData} />
+    <Vega
+      spec={VegaSpec}
+      data={{ "node-data": VegaData.nodes, "link-data": VegaData.links }}
+    />
   </div>
 );
