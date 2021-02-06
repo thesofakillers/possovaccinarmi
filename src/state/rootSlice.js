acc1848c349eed181ff5417361409d1fcd7beeef
Giulio Starace <giulio.starace@gmail.com>
2021-02-06 22:21:05 +0100

    setup react redux to work with gatsby

---
+++ src/state/rootSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const rootSlice = createSlice({
+  name: "root",
+  initialState: {
+    currentQuestion: 0,
+    ospiteRSA: false,
+    personaleRSA: false,
+    operatoreSS: false,
+  },
+  reducers: {
+    setQuestion: (state, action) => {
+      state.currentQuestion = action.payload;
+    },
+    chooseOspiteRSA: (state, action) => {
+      state.ospiteRSA = action.payload;
+    },
+    choosePersonaleRSA: (state, action) => {
+      state.personaleRSA = action.payload;
+    },
+    chooseOperatoreSS: (state, action) => {
+      state.operatoreSS = action.payload;
+    },
+  },
+});
+
+export const reducer = rootSlice.reducer;
+export const {
+  chooseOspiteRSA,
+  choosePersonaleRSA,
+  chooseOperatoreSS,
+} = rootSlice.actions;
