7b08973f0920d7b9d7a1dcf9033f5a725fdf8e61
Giulio Starace <giulio.starace@gmail.com>
2021-02-10 11:59:45 +0100

    set up basic conditional rendering

--- src/components/WizardForm.jsx
+++ src/components/WizardForm.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Question } from "./Question";
+import { Result } from "./Result";
 
 export const WizardForm = () => {
   const state = useSelector((state) => state);
 
+  const resultReady =
+    state.ospiteRSA || state.personaleRSA || state.operatoreSS;
+
   return (
     <div>
-      <Question number={state.currentQuestion} />
+      {!resultReady ? <Question number={state.currentQuestion} /> : <Result />}
     </div>
   );
 };
