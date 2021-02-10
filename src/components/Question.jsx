1e7aef453197af6200186afe292e5e0fb3993223
Giulio Starace <giulio.starace@gmail.com>
2021-02-10 12:00:20 +0100

    basic button interaction with state

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,11 +1,48 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  chooseOperatoreSS,
+  chooseOspiteRSA,
+  choosePersonaleRSA,
+  setQuestion,
+} from "../state/rootSlice";
 
 const questionNumberMap = {
-  0: "Sei un ospite di un' RSA?",
-  1: "Fai parte del personale di un' RSA?",
-  2: "Sei un operatore sanitario o sociosanitario?",
+  0: {
+    questionText: "Sei un ospite di un' RSA?",
+    questionDispatch: chooseOspiteRSA,
+  },
+  1: {
+    questionText: "Fai parte del personale di un' RSA?",
+    questionDispatch: choosePersonaleRSA,
+  },
+  2: {
+    questionText: "Sei un operatore sanitario o sociosanitario?",
+    questionDispatch: chooseOperatoreSS,
+  },
 };
 
 export const Question = (props) => {
-  return <div>{questionNumberMap[props.number]}</div>;
+  const dispatch = useDispatch();
+
+  const questionInfo = questionNumberMap[props.number];
+
+  const handleYes = (e) => {
+    e.preventDefault();
+    dispatch(questionInfo.questionDispatch(true));
+    console.log("Yes");
+  };
+  const handleNo = (e) => {
+    e.preventDefault();
+    dispatch(questionInfo.questionDispatch(false));
+    dispatch(setQuestion(props.number + 1));
+    console.log("No");
+  };
+  return (
+    <div>
+      <h2>{questionInfo.questionText}</h2>
+      <button onClick={handleYes}>Sì</button>
+      <button onClick={handleNo}>No</button>
+    </div>
+  );
 };
