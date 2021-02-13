8f0f42e9aede9680f2e9b8da74bb265b2f3894fd
Giulio Starace <giulio.starace@gmail.com>
2021-02-13 23:09:07 +0100

    add link to gov page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,5 +6,16 @@ export default () => (
   <Layout>
     <h1 className="title is-1 is-size-2-mobile">Posso Vaccinarmi?</h1>
     <WizardForm />
+    <div className="pt-4">
+      In base alle{" "}
+      <a
+        href="http://www.salute.gov.it/portale/nuovocoronavirus/dettaglioFaqNuovoCoronavirus.jsp?lingua=italiano&id=249"
+        target="_blank"
+        rel="noreferrer"
+      >
+        informazioni riportate sul sito ufficiale del Governo Italiano
+      </a>
+      .
+    </div>
   </Layout>
 );
